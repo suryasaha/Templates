fb9d4ba75158d642a48b21814be4943243163307
Surya Saha <ss2489@cornell.edu>
2014-06-01 02:39:38 -0400

    Added RO variables

--- file.sh
+++ file.sh
@@ -6,9 +6,14 @@
 set -o nounset
 set -o errexit
 
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly ARGS="$@"
+
+
 log() { # classic logger
   local prefix="[$(date +%Y/%m/%d\ %H:%M:%S)]: "
   echo "${prefix} $@" >&2
 }
 
-WDIR = `pwd`
+readonly WDIR = `pwd`
